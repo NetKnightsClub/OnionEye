2f814c35358e28cec6d5dae7fcb1c02be149a1ea
Vaughn <drkel.public.dev@proton.me>
2025-07-27 00:27:22 -0400

    UHHHHHH STORAGE... YEAHHHH-

--- tor_skimmer/storage.py
+++ tor_skimmer/storage.py
@@ -1,1 +1,7 @@
-stub
+import json
+
+def save_results_to_json(results, filename='results.json'):
+    with open(filename, 'w') as f:
+        json.dump(results, f, indent=2)
+
+# why are stubs called stubs... why am i asking these questions in comments
