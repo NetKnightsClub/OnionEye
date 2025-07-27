b894b5c9c2e05f290556e564152d160a62fb9983
Vaughn <drkel.public.dev@proton.me>
2025-07-27 00:25:42 -0400

    yeah im just importing code from my VSCode app

--- tor_skimmer/parser.py
+++ tor_skimmer/parser.py
@@ -1,1 +1,18 @@
-stub
+import re
+from urllib.parse import urljoin, urlparse
+from bs4 import BeautifulSoup
+
+def extract_links(html, base_url):
+    soup = BeautifulSoup(html, 'html.parser')
+    links = set()
+    for tag in soup.find_all('a', href=True):
+        href = tag['href'].strip()
+        full_url = urljoin(base_url, href)
+        if urlparse(full_url).scheme in ('http', 'https'):
+            links.add(full_url)
+    return list(links)
+
+def skim_page_for_js(html):
+    return bool(re.search(r'<script|javascript:', html, re.I))
+
+# ONIONS, HAVE... LAYERS!
